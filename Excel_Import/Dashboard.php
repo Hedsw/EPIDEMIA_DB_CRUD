e9a78b41c825113c26e61db9fca7a6119e794218
Yunhyeok Lee <yunhyeoklee@sav242-02c2lf.jacks.local>
2017-02-28 20:44:42 -0600

    Feb 28 Still has error at upload file to server

--- Excel_Import/Dashboard.php
+++ Excel_Import/Dashboard.php
@@ -7,113 +7,112 @@ Mysql password : eishoo6Pheis
 
 <!DOCTYPE html>
 <style>
-        @import url('http://fonts.googleapis.com/css?family=Amarante');
-      html { overflow-y: scroll; }
+    @import url('http://fonts.googleapis.com/css?family=Amarante');
+  html { overflow-y: scroll; }
 
-      ::selection { background: #5f74a0; color: #fff; }
-      ::-moz-selection { background: #5f74a0; color: #fff; }
-      ::-webkit-selection { background: #5f74a0; color: #fff; }
+  ::selection { background: #5f74a0; color: #fff; }
+  ::-moz-selection { background: #5f74a0; color: #fff; }
+  ::-webkit-selection { background: #5f74a0; color: #fff; }
 
-      /** page structure **/
-      #wrapper {
-        display: block;
-        width: 850px;
-        background: #fff;
-        margin: 0 auto;
-        padding: 10px 17px;
-        -webkit-box-shadow: 2px 2px 3px -1px rgba(0,0,0,0.35);
-      }
+  /** page structure **/
+  #wrapper {
+    display: block;
+    width: 850px;
+    background: #fff;
+    margin: 0 auto;
+    padding: 10px 17px;
+    -webkit-box-shadow: 2px 2px 3px -1px rgba(0,0,0,0.35);
+  }
 
-      #keywords {
-        margin: 0 auto;
-        font-size: 1.2em;
-        margin-bottom: 15px;
-      }
-
-
-      #keywords thead {
-        cursor: pointer;
-        background: #c9dff0;
-      }
-      #keywords thead tr th {
-        font-weight: bold;
-        padding: 12px 30px;
-        padding-left: 42px;
-      }
-      #keywords thead tr th span {
-        padding-right: 20px;
-        background-repeat: no-repeat;
-        background-position: 100% 100%;
-      }
-
-      #keywords thead tr th.headerSortUp, #keywords thead tr th.headerSortDown {
-        background: #acc8dd;
-      }
-
-      #keywords thead tr th.headerSortUp span {
-        background-image: url('http://i.imgur.com/SP99ZPJ.png');
-      }
-      #keywords thead tr th.headerSortDown span {
-        background-image: url('http://i.imgur.com/RkA9MBo.png');
-      }
+  #keywords {
+    margin: 0 auto;
+    font-size: 1.2em;
+    margin-bottom: 15px;
+  }
 
 
-      #keywords tbody tr {
-        color: #555;
-      }
-      #keywords tbody tr td {
-        text-align: center;
-        padding: 15px 10px;
-      }
-      #keywords tbody tr td.lalign {
-        text-align: left;
-      }
-      .myButton {
-    	-moz-box-shadow:inset 0px 1px 0px 0px #ffffff;
-    	-webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;
-    	box-shadow:inset 0px 1px 0px 0px #ffffff;
-    	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #f9f9f9), color-stop(1, #e9e9e9));
-    	background:-moz-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
-    	background:-webkit-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
-    	background:-o-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
-    	background:-ms-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
-    	background:linear-gradient(to bottom, #f9f9f9 5%, #e9e9e9 100%);
-    	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#f9f9f9', endColorstr='#e9e9e9',GradientType=0);
-    	background-color:#f9f9f9;
-    	-moz-border-radius:6px;
-    	-webkit-border-radius:6px;
-    	border-radius:6px;
-    	border:1px solid #ffffff;
-    	display:inline-block;
-    	cursor:pointer;
-    	color:#666666;
-    	font-family:Arial;
-    	font-size:15px;
-    	font-weight:bold;
-    	padding:6px 24px;
-    	text-decoration:none;
-    	text-shadow:0px 1px 0px #ffffff;
-      }
-    .myButton:hover {
-    	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #e9e9e9), color-stop(1, #f9f9f9));
-    	background:-moz-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
-    	background:-webkit-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
-    	background:-o-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
-    	background:-ms-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
-    	background:linear-gradient(to bottom, #e9e9e9 5%, #f9f9f9 100%);
-    	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#e9e9e9', endColorstr='#f9f9f9',GradientType=0);
-    	background-color:#e9e9e9;
-    }
-    .myButton:active {
-    	position:relative;
-    	top:1px;
-    }
+  #keywords thead {
+    cursor: pointer;
+    background: #c9dff0;
+  }
+  #keywords thead tr th {
+    font-weight: bold;
+    padding: 12px 30px;
+    padding-left: 42px;
+  }
+  #keywords thead tr th span {
+    padding-right: 20px;
+    background-repeat: no-repeat;
+    background-position: 100% 100%;
+  }
+
+  #keywords thead tr th.headerSortUp, #keywords thead tr th.headerSortDown {
+    background: #acc8dd;
+  }
+
+  #keywords thead tr th.headerSortUp span {
+    background-image: url('http://i.imgur.com/SP99ZPJ.png');
+  }
+  #keywords thead tr th.headerSortDown span {
+    background-image: url('http://i.imgur.com/RkA9MBo.png');
+  }
+
+
+  #keywords tbody tr {
+    color: #555;
+  }
+  #keywords tbody tr td {
+    text-align: center;
+    padding: 15px 10px;
+  }
+  #keywords tbody tr td.lalign {
+    text-align: left;
+  }
+  .myButton {
+	-moz-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	-webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	box-shadow:inset 0px 1px 0px 0px #ffffff;
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #f9f9f9), color-stop(1, #e9e9e9));
+	background:-moz-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+	background:-webkit-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+	background:-o-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+	background:-ms-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+	background:linear-gradient(to bottom, #f9f9f9 5%, #e9e9e9 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#f9f9f9', endColorstr='#e9e9e9',GradientType=0);
+	background-color:#f9f9f9;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #ffffff;
+	display:inline-block;
+	cursor:pointer;
+	color:#666666;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:6px 24px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #ffffff;
+  }
+.myButton:hover {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #e9e9e9), color-stop(1, #f9f9f9));
+	background:-moz-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+	background:-webkit-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+	background:-o-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+	background:-ms-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+	background:linear-gradient(to bottom, #e9e9e9 5%, #f9f9f9 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#e9e9e9', endColorstr='#f9f9f9',GradientType=0);
+	background-color:#e9e9e9;
+  }
+  .myButton:active {
+  	position:relative;
+  	top:1px;
+  }
 </style>
 <html>
   <body>
 <!-- Dashboard Table -->
   <h1>Dashboard</h1>
-<!--PDO로 연결하기 -->
   <?php
     $servername = "localhost";
     $username = "epidemiaweb_test";
@@ -125,7 +124,6 @@ Mysql password : eishoo6Pheis
       catch(PDOException $e) {
           echo "Error";
       }
-
     if(isset($_POST['submit'])) {
       try {
         $file_name = $_FILES["file"]["name"];
@@ -136,51 +134,95 @@ Mysql password : eishoo6Pheis
           $errMSG =  "Please Select Excel File";
         }
         else {
-          echo $file_name;
-          echo $file_tmp;
-        $c = 0;
+          // echo $file_name;
+          // echo $file_tmp;
         if(!isset($errMSG)) {
            $handle = fopen($file_tmp,"r");
               //File Upload Start
           try {
-            $upload_dir = $_SERVER['DOCUMENT_ROOT']."/home/leey/data/";
-            // $upload_dir = "home/leey/data";
-            $location = "home/leey/data/";
-            $location2 = $location . basename($_FILES["file"]["name"]);
-            $ExcelExt = strtolower(pathinfo($file_name,PATHINFO_EXTENSION)); // get Excel Extension
-            $valid_extensions = array('xls','csv','xlsx'); //valid extension
-            $userExcel = rand(1000,10000000).".".$ExcelExt;
-            if(in_array($ExcelExt,$valid_extensions)) {
-                if($file_size < 50000000) {
-            $moved = move_uploaded_file($file_tmp,$location2);
-            if($moved) {
-              echo "Successfuly move_uploaded_file";
-            }
-            else {
-              echo "Not uploaded because of error #".$_FILES["file"]["error"];
-            }
-          }
-          else {
-            $errMSG = "Sorry, your file is too large it should be less then 50MB";
-          }
-            }
-            else {
-                  $errMSG = "Uploaded file is empty";
-            }
-            if(!isset($errMSG))
-            {
-              $stmt = $conn->prepare("INSERT INTO FilesEpi (fileName, path) VALUES ('$file_name','$location')");
-              $stmt->bindParam('$file_name',$file_name);
+            $new_size = $file_size/1024;
+            $file = rand(1000,10000000)."-".$file_name;
+            $new_file_name = strtolower($file);
+            $final_file = str_replace(' ','-',$new_file_name);
+            $location = "/uploads";
+            $uploadfile = basename($_FILES['file']['name'][$key]);
 
-             if($stmt->execute())
-                {
-                 $successMSG = "new record succesfully inserted ...";
-                }
-             else
-               {
-                 $errMSG = "error while inserting....";
-               }
-            }//  if(!isset($errMSG))
+            $target_path = $_SERVER['DOCUMENT_ROOT']. "/data/" . basename($_FILES['file']['name']);
+            $target_path2 = "./home/leey/" . basename($_FILES['file']['name']);
+
+            // $upload_dir = $_SERVER['DOCUMENT_ROOT']."/home/leey/data/";
+            // $upload_dir = "home/leey/data";
+            // $location = "download/";
+            // $location2 = $location . basename($_FILES["file"]["name"]);
+            // $ExcelExt = strtolower(pathinfo($file_name,PATHINFO_EXTENSION)); // get Excel Extension
+            // $valid_extensions = array('xls','csv','xlsx'); //valid extension
+            // $userExcel = rand(1000,10000000).".".$ExcelExt;
+
+            $moved = move_uploaded_file($file_tmp,$target_path);
+
+            if(!$moved) {
+              //  echo "File is valid \n";
+               $stmt = $conn->prepare("INSERT INTO FilesEpi (file, type, size, path) VALUES (:file, :type, :size, :path)");
+               $stmt->bindParam(':file', $file_name);
+               $stmt->bindParam(':type', $file_type);
+               $stmt->bindParam(':size', $new_size);
+               $stmt->bindParam(':path', $file_tmp);
+               $stmt->execute();
+             }
+            else
+               echo "Error";
+            // if($moved == 0) {
+            //   echo "File is valid \n";
+            //   $stmt = $conn->prepare("INSERT INTO FilesEpi (file, type, size, path) VALUES (:file, :type, :size, :path)");
+            //   $stmt->bindParam(':file', $final_file);
+            //   $stmt->bindParam(':type', $file_type);
+            //   $stmt->bindParam(':size', $new_size);
+            //   $stmt->bindParam(':path', $file_tmp);
+            //   // $stmt->execute();
+            //  if($stmt->execute()) {
+            //       echo "File is valid2 \n"
+            //      $successMSG = "new record succesfully inserted ...";
+            // }
+            //  else
+            //      $errMSG = "error while inserting....";
+            // }
+            // else
+            //   echo "Error";
+
+
+
+
+          //   if(in_array($ExcelExt,$valid_extensions)) {
+          //       if($file_size < 50000000) {
+          //   $moved = move_uploaded_file($file_tmp,$location2);
+          //   if($moved) {
+          //     echo "Successfuly move_uploaded_file";
+          //   }
+          //   else {
+          //     echo "Not uploaded because of error #".$_FILES["file"]["error"];
+          //   }
+          // }
+          // else {
+          //   $errMSG = "Sorry, your file is too large it should be less then 50MB";
+          // }
+          //   }
+          //   else {
+          //         $errMSG = "Uploaded file is empty";
+          //   }
+            // if(!isset($errMSG))
+            // {
+            //   $stmt = $conn->prepare("INSERT INTO FilesEpi (fileName, path) VALUES ('$file_name','$location')");
+            //   $stmt->bindParam('$file_name',$file_name);
+            //
+            //  if($stmt->execute())
+            //     {
+            //      $successMSG = "new record succesfully inserted ...";
+            //     }
+            //  else
+            //    {
+            //      $errMSG = "error while inserting....";
+            //    }
+            // }//  if(!isset($errMSG))
           }
            catch(PDOException $e) {
              echo "File Upload failed: ";
@@ -190,16 +232,73 @@ Mysql password : eishoo6Pheis
              try {
                 if($handle !==FALSE) {
                   while(($filesop = fgetcsv($handle,10000,','))!==FALSE) {
-                      $id = $filesop[0];
-                      $name = $filesop[1];
-                      $population = $filesop[2];
-                      $pop_density = $filesop[3];
-                      $country = $filesop[4];
-                      $latitude = $filesop[5];
-                      $longitude = $filesop[6];
+                      $Zone = $filesop[0];
+                      $Woreda = $filesop[1];
+                      $col3 = $filesop[2];
+                      $col4 = $filesop[3];
+                      $col5 = $filesop[4];
+                      $col6 = $filesop[5];
+                      $col7 = $filesop[6];
+                      $col8 = $filesop[7];
+                      $col9 = $filesop[8];
+                      $col10 = $filesop[9];
+                      $col11 = $filesop[10];
+                      $col12 = $filesop[11];
+                      $col13 = $filesop[12];
+                      $col14 = $filesop[13];
+                      $col15 = $filesop[14];
+                      $col16 = $filesop[15];
+                      $col17 = $filesop[16];
+                      $col18 = $filesop[17];
+                      $col19 = $filesop[18];
+                      $col20 = $filesop[19];
+                      $col21 = $filesop[20];
+                      $col22 = $filesop[21];
+                      $col23 = $filesop[22];
+                      $col24 = $filesop[23];
+                      $col25 = $filesop[24];
+                      $col26 = $filesop[25];
+                      $col27 = $filesop[26];
+                      $col28 = $filesop[27];
+                      $col29 = $filesop[28];
+                      $col30 = $filesop[29];
+                      $col31 = $filesop[30];
+                      $col32 = $filesop[31];
+                      $col33 = $filesop[32];
+                      $col34 = $filesop[33];
+                      $col35 = $filesop[34];
+                      $col36 = $filesop[35];
+                      $col37 = $filesop[36];
+                      $col38 = $filesop[37];
+                      $col39 = $filesop[38];
+                      $col40 = $filesop[39];
+                      $col41 = $filesop[40];
+                      $col42 = $filesop[41];
+                      $col43 = $filesop[42];
+                      $col44 = $filesop[43];
+                      $col45 = $filesop[44];
+                      $col46 = $filesop[45];
+                      $col47 = $filesop[46];
+                      $col48 = $filesop[47];
+                      $col49 = $filesop[48];
+                      $col50 = $filesop[49];
+                      $col51 = $filesop[50];
+                      $col52 = $filesop[51];
+                      $col53 = $filesop[52];
+                      $col54 = $filesop[53];
+                      $col55 = $filesop[54];
+                      $col56 = $filesop[55];
+                      $col57 = $filesop[56];
+                      $col58 = $filesop[57];
+                      $col59 = $filesop[58];
+                      $col60 = $filesop[59];
+                      $col61 = $filesop[60];
+                      $col62 = $filesop[61];
+                      $col63 = $filesop[62];
+                      $col64 = $filesop[63];
 
-                      $stmt = $conn->prepare("INSERT INTO Data (ID,name,population,pop_density,country,latitude,longitude) VALUES ('$id','$name','$population','$pop_density','$country','$latitude','$longitude')");
-                      $stmt->execute($filesop);
+                      $stmt = $conn->prepare("INSERT INTO Data2 (Zone,Woreda,col3,col4,col5,col6,col7,col8,col9,col10,col11,col12,col13,col14,col15,col16,col17,col18,col19,col20,col21,col22,col23,col24,col25,col26,col27,col28,col29,col30,col31,col32,col33,col34,col35,col36,col37,col38,col39,col40,col41,col42,col43,col44,col45,col46,col47,col48,col49,col50,col51,col52,col53,col54,col55,col56,col57,col58,col59,col60,col61,col62,col63,col64) VALUES ('$Zone','$Woreda','$col3','$col4','$col5','$col6','$col7','$col8','$col9','$col10','$col11','$col12','$col13','$col14','$col15','$col16','$col17','$col18','$col18','$col19','$col20','$col21','$col22','$col23','$col24','$col25','$col26','$col27','$col28','$col29','$col30','$col31','$col32','$col33','$col34','$col35','$col36','$col37','$col39','$col40','$col41','$col42','$col43','$col44','$col45','$col46','$col47','$col48','$col49','$col50','$col51','$col52','$col53','$col54','$col55','$col56','$col57','$col58','$col59','$col60','$col61','$col62','$col63','$col64')");
+                      $stmt->execute();
                   }
                 }
                 else {
@@ -221,8 +320,6 @@ Mysql password : eishoo6Pheis
       $conn = null;
     }//$_post['submit']
 
-
-
 //Show Files in Here :)
 try {
     $servername = "localhost";
@@ -241,10 +338,14 @@ catch(PDOException $e) {
   <table id="keywords" cellspacing="0" cellpadding="0">
   <thead>
       <tr>
-        <th><span>FileName</span></th>
+        <!-- <th><span>FileName</span></th>
         <th><span>Upload Time</span></th>
         <th><span>Rows Uploaded</span></th>
         <th><span>Rows in Use</span></th>
+        <th><span>Download</span></th> -->
+        <th><span>FileName</span></th>
+        <th><span>type<span></th>
+        <th><span>size</span></th>
         <th><span>Download</span></th>
       </tr>
   </thead>
@@ -254,10 +355,11 @@ catch(PDOException $e) {
           foreach($result as $row) {
          ?>
          <tr>
-            <td class="lalign"><?php echo $row["fileName"]; ?></td>
-            <td>2017.02.27</td>
-            <td>3224</td>
-            <td>1234</td>
+           <!-- file, type, size, path -->
+            <td class="lalign"><?php echo $row["file"]; ?></td>
+            <!-- <td>2017.02.27</td> -->
+            <td><?php echo $row["type"]; ?></td>
+            <td><?php echo $row["size"]; ?></td>
             <td class="lalign"><a href=<?php $row["path"]?>>Download</td>
          </tr>
          <?php
@@ -266,8 +368,6 @@ catch(PDOException $e) {
      ?>
       </tbody>
   </table>
-
-
 
 <!-- File Upload  -->
   <form enctype="multipart/form-data" method="post" name="import">
--- README.md
+++ README.md
@@ -4,3 +4,117 @@
 ================
 The goal of this project is to decrease rate of illness related in mosquito.
 My Position is that develop Web App and struct Database as using RDBMS(MySQL)
+
+
+# Feb 28 Updated
+Putty 접속하는법
+ssh leey@kabru.sdstate.edu
+
+접속 비밀번호 yunhyeokLEE!
+
+mysql -u epidemiaweb_test -p epidemia_test
+
+
+Mysql password : eishoo6Pheis
+
+
+
+// 추가 해야 할 것
+// 1. Create Table하는거 해서 넣을 때 마다 테이블 만들어서 넣도록 해야함.
+// 2. 디비에 받아 놓은거 다시 뿌리는 법
+
+
+ CREATE TABLE IF NOT EXISTS `FilesEpi` (
+ `ID` int(11) NOT NULL AUTO_INCREMENT,
+ `file` VARCHAR(30) NOT NULL,
+ `type` VARCHAR(10) NOT NULL,
+ `size` INT NOT NULL,
+ `path` VARCHAR(400) NOT NULL,
+  PRIMARY KEY (`ID`) ) ;
+
+
+CREATE TABLE IF NOT EXISTS `Data2` (
+`ID2` int NOT NULL AUTO_INCREMENT,
+`Zone` VARCHAR(30) NOT NULL,
+`Woreda` VARCHAR(30) NOT NULL,
+`col3` int(20),
+`col4` int(20),
+`col5` int(20),
+`col6` FLOAT(20),
+`col7` FLOAT(20),
+`col8` FLOAT(20),
+`col9` FLOAT(20),
+`col10` FLOAT(20),
+`col11` FLOAT(20),
+`col12` FLOAT(20),
+`col13` FLOAT(20),
+`col14` FLOAT(20),
+`col15` FLOAT(20),
+`col16` FLOAT(20),
+`col17` FLOAT(20),
+`col18` FLOAT(20),
+`col19` FLOAT(20),
+`col20` FLOAT(20),
+`col21` FLOAT(20),
+`col22` FLOAT(20),
+`col23` FLOAT(20),
+`col24` FLOAT(20),
+`col25` FLOAT(20),
+`col26` FLOAT(20),
+`col27` FLOAT(20),
+`col28` FLOAT(20),
+`col29` FLOAT(20),
+`col30` FLOAT(20),
+`col31` FLOAT(20),
+`col32` FLOAT(20),
+`col33` FLOAT(20),
+`col34` FLOAT(20),
+`col35` FLOAT(20),
+`col36` FLOAT(20),
+`col37` FLOAT(20),
+`col38` FLOAT(20),
+`col39` FLOAT(20),
+`col40` FLOAT(20),
+`col41` FLOAT(20),
+`col42` FLOAT(20),
+`col43` FLOAT(20),
+`col44` FLOAT(20),
+`col45` FLOAT(20),
+`col46` FLOAT(20),
+`col47` FLOAT(20),
+`col48` FLOAT(20),
+`col49` FLOAT(20),
+`col50` FLOAT(20),
+`col51` FLOAT(20),
+`col52` FLOAT(20),
+`col53` FLOAT(20),
+`col54` FLOAT(20),
+`col55` FLOAT(20),
+`col56` FLOAT(20),
+`col57` FLOAT(20),
+`col58` FLOAT(20),
+`col59` FLOAT(20),
+`col60` FLOAT(20),
+`col61` FLOAT(20),
+`col62` FLOAT(20),
+`col63` FLOAT(20),
+`col64` FLOAT(20),
+ PRIMARY KEY (`ID2`) );
+
+
+참조 한 URL : http://stackoverflow.com/questions/19759200/how-to-save-multiple-files-in-a-chrome-app
+
+bin  boot  data  db  dev  etc  home  kabru  lib  lib64  media  mnt  opt  proc  root  
+
+로그 체크하기..
+맨 아래로 내려가서 data/epidemia/test/web/logs 로 가서 vi errors.log 하면 나옴..
+
+
+
+Feb 28
+문제는 move_uploaded_file 이건데 지금 permission Error가 뜬다. 이때는 어떻게 해야 할지
+고민 해보자
+
+Column 1 - 64
+1 - 2 are characters
+3 - 64 are numbers
